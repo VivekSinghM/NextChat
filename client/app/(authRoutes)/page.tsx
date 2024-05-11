859ed0742ef43b311c2ea3bb163b42b1a89380c8
VivekSinghM <viveksinghmongare@gmail.com>
2024-05-12 05:27:52 +0530

    feat: Add ConversationWidget component with basic styling and contact feature
    - Created ConversationWidget component with basic styled components
    - Implemented feature to add contacts.
    - Added logout option in ellipsis menu
    - TODO: Add tabs for contacts and conversations

--- client/app/(authRoutes)/page.tsx
+++ client/app/(authRoutes)/page.tsx
@@ -1,9 +1,12 @@
 'use client';
 
+import { useAuthContext } from '@/context/AuthProvider/AuthProvider';
+
 export default function Home() {
+    const { user } = useAuthContext();
     return (
         <section className='flex h-full w-full flex-row gap-1 p-2'>
-            hi from NextChat Home
+            hi from NextChat Home your id: {user.otherDetails.id}
         </section>
     );
 }
